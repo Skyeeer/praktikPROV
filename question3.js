3fe92dfe56b0a596dbe8081f430fbb0fa54ee90e
Chalel <charlie03.pl@gmail.com>
2024-02-19 10:54:29 +0100

    first commit

---
+++ question3.js
@@ -0,0 +1,18 @@
+// 3. (4p) Hämta posts med fetch från denna url: https://jsonplaceholder.typicode.com/posts
+// Visa title och body från varje post på sidan inuti: <div id="posts"></div> (skapa HTML-elementet med JavaScript eller lägg till dem i HTML-filen)
+// Visa endast posts från användaren med userId: 1
+// Använd async/await med felhantering. Använd dig av filter samt forEach eller map.
+
+
+// Din kod här
+
+async function fetch() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const fetched = await response.json();
+        console.log(fetched);
+    }
+    catch (error) {
+        console.log('something went wrong :/');
+    }
+}
